cfc541f9f1371113820c121434fd372fa12567f0
Alexander-Logvin <shura.logvin@yandex.ru>
2024-08-24 14:30:40 +0300

    Add files via upload

--- module_1_6.py
+++ module_1_6.py
@@ -3,13 +3,13 @@ print(my_dict)
 print(my_dict.get('Alex'))
 print(my_dict.get('Anton'))
 my_dict.update({'Denis': 1995, 'Artur': 1986})
-my_dict.__delitem__('Denis')
+my_dict.pop('Denis')
 print(my_dict.get('Denis'))
 print(my_dict)
 my_set = {1, 2 , 3, 4, 5, 'Alex', 1, 2, 2, 5}
 print(my_set)
 print(my_set.add(6))
-print(my_set.add('Artem')) #Выдает ошибку при добавлении сразу два значения во множество. По этому сделал двумя командамы.
+print(my_set.add('Artem')) #Выдает ошибку при добавлении сразу два значения во множество. По этому сделал двумя командами.
 print(my_set)
 print(my_set.discard(3))
 print(my_set)
